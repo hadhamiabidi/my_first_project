bbd1d19c1d2988b6a567990af3adfd1b9a300480
hadhami abidi <hadhemiabidi20@gmail.com>
2023-03-26 11:19:16 +0200

    first commit

---
+++ lib/controllers/onboarding_controller.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
+import 'package:get/utils.dart';
+
+import '../models/onboarding_info.dart';
+
+class OnboardingController extends GetxController {
+  var selectedPageIndex = 0.obs;
+  bool get isLastPage => selectedPageIndex.value == onboardingPages.length - 1;
+  var pageController = PageController();
+
+  forwardAction() {
+    if (isLastPage) {
+      //go to home page
+    } else
+      pageController.nextPage(duration: 300.milliseconds, curve: Curves.ease);
+  }
+  List<OnboardingInfo> onboardingPages = [
+    OnboardingInfo('assets/order.jpg', 'Place Your Order', 'Now you can order any time right from your mobile.'),
+    OnboardingInfo('assets/quick delivery.jpg', 'Quick Delivery', 'Orders will be immediately deliver'),
+    OnboardingInfo('assets/paiement.jpg', 'payment', 'You can pay with different methods'),
+  ];
+}
---
+++ lib/models/onboarding_info.dart
@@ -0,0 +1,7 @@
+class OnboardingInfo {
+  final ImageAsset;
+  final title;
+  final description;
+
+  OnboardingInfo(this.ImageAsset, this.title, this.description);
+}
